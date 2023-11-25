a1f770d2ed4821f0f398882618736c87ff43807f
Maftuna Tojiboeva <142628129+TinaMaftuna@users.noreply.github.com>
2023-11-25 15:29:29 -0600

    added list itself

--- rds/main.tf
+++ rds/main.tf
@@ -10,9 +10,9 @@ data "terraform_remote_state" "vpc" {
 }
 
 
+# Creates subnet group with private subnets
 resource "aws_db_subnet_group" "default" {
   name       = "main"
-  subnet_ids = [
-    data.terraform_remote_state.vpc.outputs.private_subnets
-  ]
+  subnet_ids = data.terraform_remote_state.vpc.outputs.private_subnets
 }
+

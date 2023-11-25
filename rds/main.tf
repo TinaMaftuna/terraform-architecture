a4e7b5d5e71b1a2f775050f530b1501ac43b0da0
Maftuna Tojiboeva <142628129+TinaMaftuna@users.noreply.github.com>
2023-11-25 15:26:48 -0600

    added remote state

---
+++ rds/main.tf
@@ -0,0 +1,19 @@
+data "terraform_remote_state" "vpc" {
+  backend = "remote"
+
+  config = {
+    organization = "mtojiboeva2"
+    workspaces = {
+      name = "vpc"
+    }
+  }
+}
+
+
+resource "aws_db_subnet_group" "default" {
+  name       = "main"
+  subnet_ids = [
+    data.terraform_remote_state.vpc.outputs.private_subnets
+  ]
+}
+}
